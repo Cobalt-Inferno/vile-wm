90f433934ecc7eaa854960c41854a0024f5e740d
kav <kav>
2022-09-21 19:45:49 -0400

    just removed bloat

--- vile.c
+++ vile.c
@@ -22,16 +22,19 @@ int main(void)
     XWindowAttributes attr;
     XButtonEvent start;
     XEvent ev;
+    XEvent buff;
+    Window root;
     if(!(dpy = XOpenDisplay(0x0))) return 1;
+    root = DefaultRootWindow(dpy);
     XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym("v")), Mod1Mask,
-            DefaultRootWindow(dpy), True, GrabModeAsync, GrabModeAsync);
+            root, True, GrabModeAsync, GrabModeAsync);
     XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym("f")), Mod1Mask,
-            DefaultRootWindow(dpy), True, GrabModeAsync, GrabModeAsync);
+            root, True, GrabModeAsync, GrabModeAsync);
     XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym("t")), Mod1Mask,
-            DefaultRootWindow(dpy), True, GrabModeAsync, GrabModeAsync);
-    XGrabButton(dpy, 1, Mod1Mask, DefaultRootWindow(dpy), True,
+            root, True, GrabModeAsync, GrabModeAsync);
+    XGrabButton(dpy, 1, Mod1Mask, root, True,
             ButtonPressMask|ButtonReleaseMask|PointerMotionMask, GrabModeAsync, GrabModeAsync, None, None);
-    XGrabButton(dpy, 3, Mod1Mask, DefaultRootWindow(dpy), True,
+    XGrabButton(dpy, 3, Mod1Mask, root, True,
             ButtonPressMask|ButtonReleaseMask|PointerMotionMask, GrabModeAsync, GrabModeAsync, None, None);
 
     start.subwindow = None;
@@ -49,21 +52,29 @@ int main(void)
         {
             system(load);
         }
-        else if (focusedOnWindow && ev.xkey.keycode == XKeysymToKeycode(dpy, XStringToKeysym("v")))
-            system("killall xinit");
+        else if (focusedOnWindow && ev.xkey.keycode == XKeysymToKeycode(dpy, XStringToKeysym("v"))) {
+            memset(&buff, 0, sizeof(buff));
+            buff.xclient.type = ClientMessage;
+            buff.xclient.message_type = XInternAtom(dpy, "WM_PROTOCOLS",True);
+            buff.xclient.window = ev.xkey.subwindow;
+            buff.xclient.format = 32;
+            buff.xclient.data.l[0] = XInternAtom(dpy, "WM_DELETE_WINDOW", False);
+            buff.xclient.data.l[1] = CurrentTime;
+            XSendEvent(dpy, ev.xkey.subwindow, False, 0, &buff);
+        }
 
         else if(ev.type == KeyPress && ev.xkey.subwindow != None) {
             XRaiseWindow(dpy, ev.xkey.subwindow);
         }
         else if(ev.type == ButtonPress && ev.xbutton.subwindow != None)
         {
-//            XRaiseWindow(dpy, ev.xbutton.subwindow);
+            XRaiseWindow(dpy, ev.xbutton.subwindow);
             XGetWindowAttributes(dpy, ev.xbutton.subwindow, &attr);
             start = ev.xbutton;
         }
         else if(ev.type == MotionNotify && start.subwindow != None)
         {
- //           XRaiseWindow(dpy, ev.xbutton.subwindow);
+            XRaiseWindow(dpy, ev.xbutton.subwindow);
             int xdiff = ev.xbutton.x_root - start.x_root;
             int ydiff = ev.xbutton.y_root - start.y_root;
             XMoveResizeWindow(dpy, start.subwindow,
@@ -71,10 +82,11 @@ int main(void)
                 attr.y + (start.button==1 ? ydiff : 0),
                 MAX(1, attr.width + (start.button==3 ? xdiff : 0)),
                 MAX(1, attr.height + (start.button==3 ? ydiff : 0)));
+                printf("%d %d\n",attr.x,attr.y);
         }
         else if(ev.type == ButtonRelease) {
-            start.subwindow = None;
             XUngrabPointer(dpy, CurrentTime);
+            start.subwindow = None;
         }
     }
 }
